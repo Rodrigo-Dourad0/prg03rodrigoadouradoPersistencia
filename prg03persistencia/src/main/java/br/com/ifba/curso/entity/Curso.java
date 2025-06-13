d46fb16ff20d328eb98db50bcb57a66b73eebc8e
Rodrigo Almeida Dourado <115838474+Rodrigo-Dourad0@users.noreply.github.com>
2025-06-13 19:03:35 -0300

    Cria projeto Cursos

---
+++ prg03persistencia/src/main/java/br/com/ifba/curso/entity/Curso.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.curso.entity;
+
+/**
+ *
+ * @author almei
+ */
+public class Curso {
+    private String nome;
+    private String codigo;
+    private int cargaHoraria;
+
+    // Construtor
+    public Curso(String nome, String codigo, int cargaHoraria) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    // Método para exibir informações do curso
+    public void exibirInformacoes() {
+        System.out.println("Curso: " + nome);
+        System.out.println("Código: " + codigo);
+        System.out.println("Carga Horária: " + cargaHoraria + " horas");
+    }
+}
